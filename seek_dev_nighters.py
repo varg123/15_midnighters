5451747cd67149a9f007df8878f772451a5361e0
Andrey Skilev <andreyskilev@mail.ru>
2019-04-09 11:55:09 +0500

    Написан изначальный вариант программы

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,38 @@
+import requests
+import datetime
+from pytz import timezone
+
+URL_API = 'https://devman.org/api/challenges/solution_attempts/'
+START_NIGHT_HOUR = 0
+STOP_NIGHT_HOUR = 6
+
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+      responce = requests.get(URL_API)  
+      pages_count = responce.json()['number_of_pages']
+      start_page = responce.json()['page']
+      yield from responce.json()['records']
+      for page_number in range(start_page+1,pages_count+1):
+            params={
+                  'page': page_number
+            }
+            responce = requests.get(URL_API, params=params)
+            yield from responce.json()['records']
+
 
 def get_midnighters():
-    pass
+      for attempt in load_attempts():
+            user_timezone = timezone(attempt['timezone'])
+            user_time = user_timezone.localize(datetime.datetime.fromtimestamp(attempt['timestamp']))
+            if START_NIGHT_HOUR < user_time.hour < STOP_NIGHT_HOUR:
+                  yield attempt['username']
+
+
+def main():
+      midnighters = set(get_midnighters())
+      print("Всего сов: ", len(midnighters))
+      print("А именно:")
+      for midnighter in  midnighters:
+            print(midnighter)
 
 if __name__ == '__main__':
-  pass
+      main()
